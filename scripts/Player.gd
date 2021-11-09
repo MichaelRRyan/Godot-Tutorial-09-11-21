5b1cc98b6c92c29cfe2c7808244e4b0f2a3c6f0c
C12345678 <martin.harrigan@itcarlow.ie>
2021-11-09 14:37:48 +0000

    Added basic movement and jumping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-
-var gravity = 9.8
+export var speed = 50.0
+export var gravity = 9.8
+export var jump_speed = 500.0
 
 var velocity = Vector2.ZERO
 
@@ -9,4 +10,14 @@ var velocity = Vector2.ZERO
 func _physics_process(delta):
 	velocity.y += gravity
 	
-	move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0.0
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = -jump_speed
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
